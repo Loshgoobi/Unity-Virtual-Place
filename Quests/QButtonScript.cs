062f4851b0d12869156ac3e6b690e97187c39bd3
Loshgoobi <remy.dozoul@ynov.com>
2018-01-09 23:25:52 +0100

    2 étages et double scene

---
+++ Quests/QButtonScript.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QButtonScript : MonoBehaviour {
+
+    public int questID;
+    public Text questTitle;
+
+    private GameObject acceptButton;
+    private GameObject giveupButton;
+    private GameObject completeButton;
+
+    private QButtonScript acceptButtonScript;
+    private QButtonScript giveupButtonScript;
+    private QButtonScript completeButtonScript;
+
+    private void Start()
+    {
+        //FindChild() a partir 
+        acceptButton = GameObject.Find("QuestCanvas").transform.Find("QuestPanel").transform.Find("QuestDescription").transform.Find("Buttons").transform.Find("AcceptButton").gameObject;
+        acceptButtonScript = acceptButton.GetComponent<QButtonScript>();
+
+        acceptButton = GameObject.Find("QuestCanvas").transform.Find("QuestPanel").transform.Find("QuestDescription").transform.Find("Buttons").transform.Find("GiveUpButton").gameObject;
+        acceptButtonScript = giveupButton.GetComponent<QButtonScript>();
+
+        acceptButton = GameObject.Find("QuestCanvas").transform.Find("QuestPanel").transform.Find("QuestDescription").transform.Find("Buttons").transform.Find("CompleteButton").gameObject;
+        acceptButtonScript = completeButton.GetComponent<QButtonScript>();
+
+        acceptButton.SetActive(false);
+        giveupButton.SetActive(false);
+        completeButton.SetActive(false);
+
+    }
+
+    //SHOW ALL INFOS
+    public void ShowAllInfos()
+    {
+        QuestUIManager.uiManager.ShowSelectedQuest(questID);
+        //ACCEPT BUTTON
+        if(QuestManager.questManager.RequestAvailableQuest(questID))
+        {
+            acceptButton.SetActive(true);
+            acceptButtonScript.questID = questID;
+        }
+        else
+        {
+            acceptButton.SetActive(false);
+        }
+
+        //GIVE UP BUTTON
+        if (QuestManager.questManager.RequestAvailableQuest(questID))
+        {
+            giveupButton.SetActive(true);
+            giveupButtonScript.questID = questID;
+        }
+        else
+        {
+            giveupButton.SetActive(false);
+        }
+
+        //COMPLETE BUTTON
+        if (QuestManager.questManager.RequestAvailableQuest(questID))
+        {
+            completeButton.SetActive(true);
+            completeButtonScript.questID = questID;
+        }
+        else
+        {
+            completeButton.SetActive(false);
+        }
+    }
+
+    public void AcceptQuest()
+    {
+        QuestManager.questManager.AcceptQuest(questID);
+        QuestUIManager.uiManager.HideQuestPanel();
+
+
+        //UPDATE ALL NPCS
+        QuestObject[] currentQuestGuys = FindObjectsOfType(typeof(QuestObject)) as QuestObject[];
+        foreach(QuestObject obj in currentQuestGuys)
+        {
+           // obj.setQuestMarker();
+        }
+
+    }
+
+    public void GiveUpQuest()
+    {
+        QuestManager.questManager.GiveUpQuest(questID);
+        QuestUIManager.uiManager.HideQuestPanel();
+
+
+        //UPDATE ALL NPCS
+        QuestObject[] currentQuestGuys = FindObjectsOfType(typeof(QuestObject)) as QuestObject[];
+        foreach (QuestObject obj in currentQuestGuys)
+        {
+            //obj.setQuestMarker();
+        }
+
+    }
+
+    public void CompleteQuest()
+    {
+        QuestManager.questManager.CompleteQuest(questID);
+        QuestUIManager.uiManager.HideQuestPanel();
+
+
+        //UPDATE ALL NPCS
+        QuestObject[] currentQuestGuys = FindObjectsOfType(typeof(QuestObject)) as QuestObject[];
+        foreach (QuestObject obj in currentQuestGuys)
+        {
+          //  obj.setQuestMarker();
+        }
+
+    }
+
+    public void ClosePanel()
+    {
+        QuestUIManager.uiManager.HideQuestPanel();
+    }
+
+
+
+}
---
+++ Quests/QuestObject.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script to apply to the object used in the quest
+/// </summary>
+public class QuestObject : MonoBehaviour {
+
+    private bool inTrigger = false;
+
+    public List<int> availableQuestIDs = new List<int>();
+    public List<int> receivableQuestIDs = new List<int>();
+
+    public GameObject questMarker;
+    public Image theImage;
+
+    public Sprite questAvailableSprite;
+    public Sprite questReceivableSprite;
+
+    // Use this for initialization
+    void Start ()
+    {
+       // setQuestMarker();
+	}
+	
+   /* public void setQuestMarker()
+    {
+        if(QuestManager.questManager.CheckCompletedQuest(this))
+        {
+            questMarker.SetActive(true);
+            theImage.sprite = questReceivableSprite;
+            theImage.color = Color.yellow;
+        }
+        else if(QuestManager.questManager.CheckAvailableQuest(this))
+        {
+            questMarker.SetActive(true);
+            theImage.sprite = questAvailableSprite;
+            theImage.color = Color.yellow;
+        }
+        else if(QuestManager.questManager.CheckAvailableQuest(this))
+        {
+            questMarker.SetActive(true);
+            theImage.sprite = questReceivableSprite;
+            theImage.color = Color.gray;
+        }
+    }*/
+	// Update is called once per frame
+	void Update ()
+    {
+		if (inTrigger && Input.GetKeyDown(KeyCode.F))
+        {
+            //quest ui manager
+            QuestUIManager.uiManager.CheckQuests(this);
+            Debug.Log("questsss");
+            //QuestManager.questManager.QuestRequest(this);
+        }
+	}
+
+    void onTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            inTrigger = true;
+        }
+    }
+    void onTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inTrigger = false;
+        }
+    }
+}
